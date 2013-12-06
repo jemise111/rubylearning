0970b1ec3b1f359fd016eed02571402b727579be
Victor Goff <keeperotphones@gmail.com>
2013-12-05 23:51:43 -0600

    Style and edgecase prints for 1 and 0
    Obviously the edgecases aren't working.

--- 1wk/3c_text_formatting.rb
+++ 1wk/3c_text_formatting.rb
@@ -1,29 +1,30 @@
 # 3c_text_formatting.rb
-# Challenge: write a program to display a multiplication table according to the guidelines given
+# Challenge: write a program to display a multiplication table according to the
+# guidelines given
 
-def multiplication_table n, heading = '', decorate = false
+def multiplication_table(n, heading = '', decorate = false)
   table = ''
   n.times do |row|
     row += 1                # since index starts at 0
     n.times do |column|
-      column += 1           
+      column += 1
       width = (column * n).to_s.size
-      table<<sprintf(" %*s", width, row * column)
+      table << sprintf(' %*s', width, row * column)
     end
-    table<<"\n"
+    table << "\n"
   end
   if decorate
-    table.prepend("=" * get_width(n) + "=\n")
-    table<<"=" * get_width(n) + "="
+    table.prepend('=' * get_width(n) + "=\n")
+    table << '=' * get_width(n) + '='
   end
   table.prepend(heading.center(get_width(n)) + "\n")
   table
 end
 
-def get_width n       # returns width of the last (and largest) row
+def get_width(n)      # returns width of the last (and largest) row
   width = 0
   n.times do |i|
-    i += 1                  
+    i += 1
     width += (i * n).to_s.size + 1
   end
   width
@@ -31,7 +32,14 @@ end
 
 table1 = multiplication_table(9, 'Times Table to 9', true)
 table2 = multiplication_table(20)
+table3 = multiplication_table(0)
+table4 = multiplication_table(1)
 
 puts table1
 puts
 puts table2
+puts
+puts table3
+puts
+puts table4
+
