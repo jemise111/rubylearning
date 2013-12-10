6f6723b6e69949f64447f2e906f8c652b1dd012d
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-09 23:33:03 -0500

    expand variable name

--- 2wk/1e_linify.rb
+++ 2wk/1e_linify.rb
@@ -2,5 +2,5 @@
 # Write a program that processes a string a line at a time
 
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
-num_lines = s.split("\n").size
-num_lines.times { |l| puts "Line #{l.next}: #{s.split("\n")[l]}"}
+number_of_lines = s.split("\n").size
+number_of_lines.times { |i| puts "Line #{i.next}: #{s.split("\n")[i]}"}
