3dfd7a716337f1c773bd8c5d26d5d98e590e41ab
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-10 11:05:39 -0500

    refactor and add faulty test
    define method to allow for testing
    define array to improve efficiency
    use method each_with_index

--- 2wk/1e_linify.rb
+++ 2wk/1e_linify.rb
@@ -1,6 +1,19 @@
 # 1e_linify.rb
 # Write a program that processes a string a line at a time
 
+=begin
+doctest: process string line by line using line numbers
+>> linify("a\nb\nc")
+=> "Line 1: a\nLine 2: b\nLine 3: c"
+=end
+def linify string
+  individual_lines = string.split("\n")
+  result = ''
+  individual_lines.each_with_index do |line, i|
+    result <<"Line #{i.next}: #{line}\n"
+  end
+  result
+end  
+
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
-number_of_lines = s.split("\n").size
-number_of_lines.times { |i| puts "Line #{i.next}: #{s.split("\n")[i]}"}
+puts linify(s)
