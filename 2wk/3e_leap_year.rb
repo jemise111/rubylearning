341882a75312e17d5e5e69270f0d39ecae2624a9
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-10 11:30:03 -0500

    refactor leap_year
    remove unnecessary 'true' and 'false' in conditionals
    use loop do instead of while loop
    allow for negative years
    add year validation to leap_year? using return string to flag invlid year

--- 2wk/3e_leap_year.rb
+++ 2wk/3e_leap_year.rb
@@ -4,29 +4,36 @@
 
 =begin
 doctest: return true if a year is a leap year
->> [400, 1896, 2000].map { |y| leap_year?(y) }.all?
+>> [-400, -1896, -2000, 400, 1896, 2000].map { |y| leap_year?(y) }.all?
 => true
 doctest: return false if a year is not a leap year
->> [300, 1900, 2001].map { |y| leap_year?(y) }.all?
+>> [-300, -1900, -2001, 0, 300, 1900, 2001].map { |y| leap_year?(y) }.all?
 => false
+doctest: return error string if a year isn't valid
+>> [0, 'cat'.to_i].map { |y| leap_year?(y) }.all?
+=> 'error'
 =end
 def leap_year? year
-  if year % 4 == 0
-    year % 100 == 0 ? year % 400 == 0 : true
+  unless year == 0
+    if year % 4 == 0
+      year % 100 == 0 ? year % 400 == 0 : true
+    end
   else
-    false
+    'error'
   end
 end
 
 MINUTES_IN_A_STANDARD_YEAR =      60 * 24 * 365
 MINUTES_IN_A_LEAP_YEAR     =      60 * 24 * 366
 print "Please enter a valid year: "
-while true
-  year = gets.chomp.to_i
-  if year > 0
-    puts "There are #{leap_year?(year) ? MINUTES_IN_A_LEAP_YEAR : MINUTES_IN_A_STANDARD_YEAR} minutes in the year #{year}."
-    break
+loop do
+  year = gets.to_i
+  if leap_year?(year) == 'error'
+    puts 'Numeric characters only please. Remember 0 is not a year.'
+    print 'Please try again: '
   else
-    print "A valid year is a positive number. Please try again: "
+    minutes = leap_year?(year) ? MINUTES_IN_A_LEAP_YEAR : MINUTES_IN_A_STANDARD_YEAR
+    puts "There are #{minutes} minutes in the year #{year}."
+    break
   end
 end
