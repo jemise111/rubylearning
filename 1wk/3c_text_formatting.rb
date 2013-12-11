64d8d6183ee966a512e985103b2b8fd2b60cc47d
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-10 20:36:09 -0500

    Format indentation for rdoc

--- 1wk/3c_text_formatting.rb
+++ 1wk/3c_text_formatting.rb
@@ -15,14 +15,14 @@ def prepare_result_string
 end
 
 =begin
-doctest: returns a multiplication table for n with a space in the first column
->> @table = ''
->> build_the_table(0)
-=> " 0\n"
->> build_the_table(1)
-=> " 1\n"
->> build_the_table(2)
-=> " 1 2\n 2 4\n"
+    doctest: returns a multiplication table for n with a space in the first column
+    >> @table = ''
+    >> build_the_table(0)
+    => " 0\n"
+    >> build_the_table(1)
+    => " 1\n"
+    >> build_the_table(2)
+    => " 1 2\n 2 4\n"
 =end
 def build_the_table n
    if n == 0
@@ -42,10 +42,10 @@ def build_the_table n
 end
 
 =begin
-doctest: decorates table with equal signs
->> @table = " 1 2\n 2 4\n"
->> decoration(2)
-=> "=====\n 1 2\n 2 4\n====="
+    doctest: decorates table with equal signs
+    >> @table = " 1 2\n 2 4\n"
+    >> decoration(2)
+    => "=====\n 1 2\n 2 4\n====="
 =end
 def decoration n
   @table.prepend('=' * get_width(n) + "=\n")
@@ -53,19 +53,19 @@ def decoration n
 end
 
 =begin
-doctest: prepends table with centered heading
->> @table = " 1 2\n 2 4\n"
->> heading(2, "m 2")
-=> " m 2 \n 1 2\n 2 4\n"
+    doctest: prepends table with centered heading
+    >> @table = " 1 2\n 2 4\n"
+    >> heading(2, "m 2")
+    => " m 2 \n 1 2\n 2 4\n"
 =end
 def heading n, heading
   @table.prepend(heading.center(get_width(n) + 1) + "\n")   # add 1 to width for trailing "="   
 end
 
 =begin
-doctest: returns width of last (and largest row in the table)
->> get_width(4)
-=> 10
+    doctest: returns width of last (and largest row in the table)
+    >> get_width(4)
+    => 10
 =end
 def get_width(n)      # returns width of the last (and largest) row
   width = 0
