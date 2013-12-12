04a76bcb67276778067eb1ff3553511f166b08ef
Victor Goff <keeperotphones@gmail.com>
2013-12-11 23:05:06 -0600

    using each_line, with_index and map
    Updated test to reflect the lack of newline in result of linify
    method.

--- 2wk/1e_linify.rb
+++ 2wk/1e_linify.rb
@@ -4,16 +4,13 @@
 =begin
 doctest: process string line by line using line numbers
 >> linify ("a\nb\nc")
-=> "Line 1: a\nLine 2: b\nLine 3: c\n"
+=> "Line 1: a\nLine 2: b\nLine 3: c"
 =end
 def linify string
-  individual_lines = string.split("\n")
-  result = ''
-  individual_lines.each_with_index do |line, i|
-    result << "Line #{i.next}: #{line}\n"
-  end
-  result
-end  
+  string.each_line.with_index(1).map do |line, i|
+    "Line #{i}: #{line}"
+  end.join
+end
 
 s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
 puts linify(s)
