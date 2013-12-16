8561db4ad5636ee5f7a65334de27f304333b9a56
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-16 00:11:39 -0500

    add attempt at exercise 2 and test text file

---
+++ 3wk/2e_text_target.rb
@@ -0,0 +1,43 @@
+# 2e_text_target.rb
+# Write a program to open the file 2e_text.txt and insert word
+# 'inserted' before the word 'word'
+
+=begin
+    doctest: if string contains 'word' insert 'insert' and return string
+    >> line = 'text text text'
+    >> conditional_insert(line)
+    => 'text text text'
+    >> line = 'text word text'
+    >> conditional_insert(line)
+    =>'text insert word text'
+=end
+def conditional_insert line
+  if !line.include?('insert')
+    line.gsub('word', 'insert word')
+  else
+    line
+  end
+end
+
+=begin
+    doctest: open a file and return manipulated lines as string
+    >> file = '2e_text.txt'
+    >> file_lines_to_array(file)
+    => "
+        text text text text text\n
+        text text text text text\n
+        text text insert word text text\n
+        text text text text text\n
+        text text text text text\n
+        "
+=end
+def file_lines_to_array file
+  File.open(file, 'r').each_line.map do |line|
+    conditional_insert(line)
+  end.join
+end
+
+if __FILE__ == $PROGRAM_NAME
+  file = ARGV[0]
+  File.open(file, 'r+') { |f| f.write(file_lines_to_array(file)) }
+end
