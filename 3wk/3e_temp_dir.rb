47e7c3d384693571d014b28d14fcfcf7c42674ee
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-16 10:33:49 -0500

    change variable name for accuracy

--- 3wk/3e_temp_dir.rb
+++ 3wk/3e_temp_dir.rb
@@ -2,8 +2,8 @@
 # Use class Dir to complete exercise
 
 dir_name = 'tmp'
-puts current_dir =  Dir.pwd
+puts start_dir =  Dir.pwd
 Dir.mkdir(dir_name) if !Dir.exists?(dir_name)
 Dir.chdir(dir_name) { puts Dir.pwd }
-Dir.chdir(current_dir) 
+Dir.chdir(start_dir) 
 Dir.delete(dir_name)
