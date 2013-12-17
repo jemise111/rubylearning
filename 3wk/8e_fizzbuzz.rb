7ec773294a6bfa4a94e1c44204d1893e6f607c30
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-17 09:10:18 -0500

    add first attempt at exercise 8"

---
+++ 3wk/8e_fizzbuzz.rb
@@ -0,0 +1,23 @@
+# 8e_fizzbuzz.rb
+# Print 1-100 substitution 'Fizz' for multiples of 3 and 'Buzz' for
+# multiples of 5
+
+=begin
+    doctest: return fizzbuzz string for integer
+    >> fizzbuzz(3)
+    => 'Fizz'
+    >> fizzbuzz(5)
+    => 'Buzz'
+    >> fizzbuzz(15)
+    => 'FizzBuzz'
+=end
+def fizzbuzz integer
+  result = ''
+  result << 'Fizz' if integer % 3 == 0
+  result << 'Buzz' if integer % 5 == 0 
+  result
+end
+
+1.upto(100) do |n|
+  puts "#{n % 3 == 0 || n % 5 == 0 ? fizzbuzz(n) : n}"
+end
