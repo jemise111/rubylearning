fd0282edf6887f029f0c137e470b8eb52fe0b66d
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-17 08:42:58 -0500

    refactor exercise 3
    change if !exist? to unless exist? to improve readability

--- 3wk/3e_temp_dir.rb
+++ 3wk/3e_temp_dir.rb
@@ -3,7 +3,7 @@
 
 dir_name = 'tmp'
 puts start_dir =  Dir.pwd
-Dir.mkdir(dir_name) if !Dir.exists?(dir_name)
+Dir.mkdir(dir_name) unless Dir.exists?(dir_name)
 Dir.chdir(dir_name) { puts Dir.pwd }
 Dir.chdir(start_dir) 
 Dir.delete(dir_name)
