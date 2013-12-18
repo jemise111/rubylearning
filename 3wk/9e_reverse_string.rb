280283df32dde968b9f3f58b977f84f5191a9e79
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-17 20:49:26 -0500

    Split Word
    Documented perceived problems with lines.
    
    We found out that using a space in a string will split newlines as well
    as any other whitespace.  We went to the dcoumentation to find this out.

--- 3wk/9e_reverse_string.rb
+++ 3wk/9e_reverse_string.rb
@@ -3,13 +3,20 @@
 
 =begin
     doctest: reverse word order of a string
-    >> reverse_word_order('This is a test.')
-    => 'test. a is this'
+    >> s = "This is test line one."
+    >> reverse_word_order(s)
+    => "one. line test is This"
+    doctest: Lines are handled this way
+    >> s = "This is test line one.\nThis is test line two."
+    >> reverse_word_order(s)
+    => "two. line test is This one. line test is This"
 =end
 def reverse_word_order string
-  word_array = string.split
-  word_array.reverse.join(' ')
+  string.split.reverse.join(' ')
 end
 
-test_string = 'This is a test string. Will it appear in reverse?'
-puts reverse_word_order(test_string)
+if __FILE__ == $PROGRAM_NAME
+  test_string = "This is a test string.\nThis line should be returned first."
+  puts reverse_word_order(test_string)
+end
+
