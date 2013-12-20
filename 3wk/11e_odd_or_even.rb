9a63fcbac79b45aca1021044ab6d1264aa2f7017
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-20 14:08:02 -0500

    add exercise 11, odd or even

---
+++ 3wk/11e_odd_or_even.rb
@@ -0,0 +1,19 @@
+# 11e_odd_or_even.rb
+# Given array of numbers, print each number and say whether it is
+# odd or even.
+
+=begin
+    doctest: return 'odd' or 'even' for given integer
+    >> odd_or_even(4)
+    => 'even'
+    >> odd_or_even(-3)
+    => 'odd'
+=end
+def odd_or_even integer
+  integer % 2 == 0 ? 'even' : 'odd'
+end
+
+if __FILE__ == $PROGRAM_NAME
+  collection = [12, 23, 456, 123, 4579]
+  collection.each { |n| puts "#{n} is #{odd_or_even(n)}" }
+end
