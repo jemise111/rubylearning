62114038a52799936c74cb3054f526476419d243
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-20 22:56:55 -0500

    add exercise 16, testing sort and sort_by methods

---
+++ 3wk/16e_sort_by_length.rb
@@ -0,0 +1,23 @@
+# 16e_sort_by_length.rb
+# Which of the following will do an ascending sort by string length.
+
+=begin
+    doctest: sort array ascendingly by string length
+    >> a = ['bbb', 'a', 'cc']
+    >> a.sort { |l, r| l.length <=> r.length }
+    => ['a', 'cc', 'bbb']
+    >> a.sort_by { |s| s.length }
+    => ['a', 'cc', 'bbb']
+    doctest: sort array alphabetically
+    >> a = ['bbb', 'a', 'cc']
+    >> a.sort
+    => ['a', 'bbb', 'cc']
+    >> a.sort { |l, r| l <=> r }
+    => ['a', 'bbb', 'cc']
+    >> a.sort_by { |s| s }
+    doctest: sort passing block with one argument will throw an error
+    >> a = ['bbb', 'a', 'cc']
+    >> a.sort { |s| s }
+    => ['a', 'bbb', 'cc']
+=end
+
