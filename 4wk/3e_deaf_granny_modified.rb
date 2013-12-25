9ba7c94203361ca6fc94b0343dc6fe90fd3d2dab
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-25 17:05:52 -0500

    edit for styling
    use parens in method definitions
    note require statement looks ugly

--- 4wk/3e_deaf_granny_modified.rb
+++ 4wk/3e_deaf_granny_modified.rb
@@ -1,6 +1,7 @@
 # 6e_deaf_granny.rb
 # Write a deaf granny program according to the given guidelines
 
+# this looks ugly but require_relative caused failing tests
 require "#{Dir.pwd}/../2wk/1c_prompt.rb"
 
 =begin
@@ -14,12 +15,12 @@ require "#{Dir.pwd}/../2wk/1c_prompt.rb"
         ].map { |s| shouted?(s) }.all?
     => false
 =end
-def shouted? string
+def shouted?(string)
   string == string.upcase
 end
 
 =begin
-    doctest: if shouted returns granny exclamation, otherwise 
+    doctest: if shouted returns granny exclamation, otherwise
              told to speak up
     >> granny_response('Hi')
     => 'HUH?! SPEAK UP, SONNY!'
@@ -27,7 +28,7 @@ end
     >> granny_response('HI')
     => "NO, NOT SINCE 1950!"
 =end
-def granny_response say_to_granny
+def granny_response(say_to_granny)
   if shouted?(say_to_granny)
     "NO, NOT SINCE #{1930 + rand(20)}!"
   else
@@ -38,17 +39,17 @@ end
 =begin
     doctest: return false if doesn't receive n consecutive 'BYE's
     >> @bye_counter = 0
-    >> ['BYE', 'Hi', 'BYE', 
+    >> ['BYE', 'Hi', 'BYE',
         'BYE', 'BYE'].map { |s| consecutive_bye(3, s) }
     => [false, false, false, false, true]
 =end
-def consecutive_bye n, string
+def consecutive_bye(n, string)
   string == 'BYE' ? @bye_counter += 1 : @bye_counter = 0
   @bye_counter >= n
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Hello Sonny!  Please do speak loudly, my durablecells have died." 
+  puts 'Hello Sonny!  Please do speak loudly, my durablecells have died.'
   @bye_counter = 0
   question = 'What would you like to say to deaf granny?'
   until consecutive_bye(3, say_to_granny = prompt(question, 1))
