2d9156ab1492c33b6d3de068e0e4089663b2c7c8
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-27 17:48:22 -0500

    use more accurate method name

--- 4wk/inventory.rb
+++ 4wk/inventory.rb
@@ -2,10 +2,10 @@
 # write one-line Ruby script that displays on screen all files in current
 # folder and everything in all sub folders
 
-def list_all
+def list_all_in_current_directory
   Dir.glob('**/*').sort
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts list_all
+  puts list_all_in_current_directory
 end
