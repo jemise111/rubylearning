d7eee382190c87a06855bcd4328d275cee04ec33
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-26 23:25:49 -0500

    add testing for exercise 4, swap contents

--- test/p028swapcontents_spec.rb
+++ test/p028swapcontents_spec.rb
@@ -1,14 +1,25 @@
 require 'minitest/autorun'
 require_relative '../4wk/p028swapcontents'
 
-describe 'file swap' do
+describe 'file_swap' do
   before :each do
-    File.new('file_a.txt', 'w') { |f| f.puts('test a') }
-    File.new('file_b.txt', 'w') { |f| f.puts('test b') }
+    File.open('file_a.txt', 'w') { |f| f.write('this is test a') }
+    File.open('file_b.txt', 'w') { |f| f.write('this is test b') }
   end
-  
+
+  after :each do
+    File.delete('file_a.txt')
+    File.delete('file_b.txt')
+  end
+
   it 'must copy contents from one file to another' do
     copy_contents('file_a.txt', 'file_b.txt')
-    File.open('file_b.txt').readlines.join.must_match(/a/)
+    File.open('file_b.txt').read.must_match(/a/)
+  end
+
+  it 'must swap contents from two files' do
+    swap_contents('file_a.txt', 'file_b.txt')
+    File.open('file_a.txt').read.must_match(/b/)
+    File.open('file_b.txt').read.must_match(/a/)
   end
 end
