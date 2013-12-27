670b25d0e38d8a29c54ee1815e9f07479ed4a784
Jesse Sessler <jesse.sessler@gmail.com>
2013-12-27 15:48:57 -0500

    add challenge exercise 1, failing test
    teach trick not recognizing name variable

---
+++ 4wk/1c_dog_teach_trick.rb
@@ -0,0 +1,66 @@
+# 1e_dog.rb
+# Write class Dog, that has name instance variable and 3 given methods.
+
+class Dog
+  def initialize(name)
+    @name = name
+    reset_hunger
+  end
+
+  def teach_trick(method_name)
+    self.define_singleton_method(method_name) do
+      yield
+    end
+  end
+
+  def bark
+    increment_hunger
+    'RUFF RUFF!'
+  end
+
+  def chase_cat
+    if hungry?
+      "#{@name} sees a cat but doesn't have the energy to chase it. He could use some food."
+    else
+      increment_hunger
+      "#{@name} sees a cat and chases it! Good thing he's a friendly dog."
+    end
+  end
+
+  def eat
+    answer = if hungry?
+               "#{@name} chomps away. Boy was he hungry."
+             else
+               "#{@name} snacks a bit. He leaves some food left over."
+             end
+    reset_hunger && answer
+  end
+
+  private
+
+  def hungry?
+    @hunger_level > 3
+  end
+
+  def reset_hunger
+    @hunger_level = 0
+  end
+
+  def increment_hunger
+    @hunger_level += 1
+  end
+
+  def method_missing(method, *args, &block)
+    @name.send(method, *args, &block)
+  rescue NoMethodError
+    "#{@name} doesn't know how to #{method}"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  leo = Dog.new('leo')
+  leo.teach_trick(:say_name) { "My name is #{@name.inspect}" }
+  puts leo.say_name
+  fido = Dog.new('fido')
+  puts fido.sit
+end
