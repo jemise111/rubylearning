38ced3ffbd65f5d119c308851f783d28046ded87
Jesse Sessler <jesse.sessler@gmail.com>
2014-01-06 23:59:59 -0500

    allow teach_trick to use instance variable
    define despace for String class to allow for multi line string
    use $stderr to allow testing of method_missing
    teach count trick to demonstrate sleep method
    prefer ternary operator

--- 4wk/1c_dog_teach_trick.rb
+++ 4wk/1c_dog_teach_trick.rb
@@ -1,16 +1,12 @@
 # 1c_dog_teach_trick.rb
-# Write method teach_trick that allows user to teach a trick to an instance of dog.
+# Write method teach_trick that allows user to teach a trick to an
+# instance of dog.
 
 class Dog
+  attr_reader :name
   def initialize(name)
     @name = name
     reset_hunger
-  end
-
-  def teach_trick(method_name)
-    define_singleton_method(method_name) do
-      yield
-    end
   end
 
   def bark
@@ -20,7 +16,10 @@ class Dog
 
   def chase_cat
     if hungry?
-      "#{@name} sees a cat but doesn't have the energy to chase it. He could use some food."
+      <<-EOS.despace
+      #{@name} sees a cat but doesn't have the energy to chase it.
+        He could use some food.
+      EOS
     else
       increment_hunger
       "#{@name} sees a cat and chases it! Good thing he's a friendly dog."
@@ -28,12 +27,16 @@ class Dog
   end
 
   def eat
-    answer = if hungry?
-               "#{@name} chomps away. Boy was he hungry."
-             else
-               "#{@name} snacks a bit. He leaves some food left over."
-             end
+    answer = hungry? ?
+      "#{@name} chomps away. Boy was he hungry." :
+      "#{@name} snacks a bit. He leaves some food left over."
     reset_hunger && answer
+  end
+
+  def teach_trick(method_name, *args, &blk)
+    define_singleton_method(method_name, blk) do
+      yield(*args) if block_given?
+    end
   end
 
   private
@@ -51,16 +54,43 @@ class Dog
   end
 
   def method_missing(method, *args, &block)
-    @name.send(method, *args, &block)
-  rescue NoMethodError
-    "#{@name} doesn't know how to #{method}"
+    $stderr.puts "#{@name} doesn't know how to #{method}"
+  end
+
+end
+
+class String
+  def despace
+    split("\n").map {|e| e.strip }.join(' ')
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  leo = Dog.new('leo')
-  leo.teach_trick(:say_name) { "My name is #{@name}" }
-  puts leo.say_name
-  fido = Dog.new('fido')
+  leo = Dog.new('Leo')
+  leo.teach_trick(:say_name) do |dog, color='brown'|
+    "My name is #{dog.name} and his color is #{color}"
+  end
+
+  pluto = Dog.new('Pluto')
+  pluto.teach_trick(:count) do |min = 0, last = 3|
+  def stagger(min, last)
+    min.upto(last) do |n|
+      puts n
+      sleep 0.5
+    end
+  end
+  print "Let me start: "
+  stagger(min, last)
+  puts "Yay!"
+  end
+  pluto.count
+  puts leo.say_name(leo, 'purple')
+  fido = Dog.new('Fido')
+  fido.count(5)
   puts fido.sit
+  3.times do
+    puts fido.bark
+    puts fido.chase_cat
+  end
 end
+
--- test/1c_dog_teach_trick_spec.rb
+++ test/1c_dog_teach_trick_spec.rb
@@ -10,12 +10,13 @@ describe Dog do
     @dog.teach_trick(:dance) { "The dog is dancing!" }
     @dog.must_respond_to(:dance)
     Dog.new('Leo').wont_respond_to(:dance)
-    Dog.new('Leo').dance.must_match(/doesn't know how to/)
+    # must_output takes argument for which pipe, nil to check stderr
+    -> {Dog.new('Leo').dance}.must_output(nil, /doesn't know how to/)
   end
 
   it 'must learn to say name' do
-    @dog.teach_trick(:say_name) { "My name is #{@name}" }
+    @dog.teach_trick(:say_name, @dog) {|dog| "My name is #{dog.name}" }
     @dog.must_respond_to(:say_name)
-    @dog.say_name.must_match(/Fido/)
+    @dog.say_name(@dog).must_match(/Fido/)
   end
 end
