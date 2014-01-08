fea9ebe721bda5f73cdb7789001e71e4e4025998
Jesse Sessler <jesse.sessler@gmail.com>
2014-01-08 00:09:40 -0500

    remove unnecessary puts in testing

--- test/1c_quick_repl_spec.rb
+++ test/1c_quick_repl_spec.rb
@@ -3,8 +3,8 @@ require_relative '../5wk/1c_quick_repl'
 
 describe 'quick repl' do
   it 'must display return of expression' do
-    expression = "puts 'hi'"
-    evaluate(expression).must_equal('nil')
+    expression = "x = 'hi'"
+    evaluate(expression).must_match(/hi/)
     expression = '3 > 2 && 5 - 1 < 8'
     evaluate(expression).must_equal('true')
     expression = '6 * 4'
