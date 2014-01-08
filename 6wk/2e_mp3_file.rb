3beb7bb64334011b32f4ae1ebec896d7f59e4e89
Jesse Sessler <jesse.sessler@gmail.com>
2014-01-08 00:10:42 -0500

    add exercise 2, mp3 file
    add 4 test songs in test_songs directory
    add testing for exercise 2

---
+++ 6wk/2e_mp3_file.rb
@@ -0,0 +1,29 @@
+# 2e_mp3_file.rb
+# Write a program that analyzes an mp3 file according to the specifications
+
+class UnreadableSongError < RuntimeError
+end
+
+def song_info(id3_tag)
+  fail UnreadableSongError unless tag_defined?(id3_tag)
+  encoded_information = id3_tag[3..-1].unpack('A30' * 3)
+  song_info_keys = [:title, :artist, :album]
+  Hash[song_info_keys.zip encoded_information]
+end
+
+def tag_defined?(id3_tag)
+  id3_tag[0, 3] == 'TAG'
+end
+
+if __FILE__ == $PROGRAM_NAME
+  begin
+    id3_tag = IO.read(ARGV[0]).byteslice(-128..-1)
+    song_info(id3_tag).each do |k, v|
+      puts "#{k.to_s.capitalize}: #{v}"
+    end
+  rescue UnreadableSongError
+    puts 'Song file in unreadable format'
+  rescue Exception # if no file given or other problems
+    puts 'usage: 2e_mp3_file.rb <song_file>'
+  end
+end
---
+++ test/2e_mp3_file_spec.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require_relative '../6wk/2e_mp3_file'
+
+describe 'mp3 file' do
+  before :each do
+    song_file = 'test_songs/song.mp3'
+    faulty_file = '1wk/2c_convert.rb'
+    @tag_actual = File.open(song_file, 'r').readlines.join.byteslice(-128..-1)
+    @tag_faulty = File.open(faulty_file, 'r').readlines.join.byteslice(-128..-1)
+  end
+
+  it 'must recognize a valid id3 tag' do
+    tag_defined?(@tag_actual).must_equal(true)
+    tag_defined?(@tag_faulty).wont_equal(true)
+  end
+
+  it 'must raise an error when tag is faulty' do
+    expected = -> { song_info(@tag_faulty) }
+    expected.must_raise(UnreadableSongError)
+  end
+
+  it 'must create a hash containing song information' do
+    expected = { title: 'Dancing Shoes',
+                 artist: 'Cliff Richard and The Shadows',
+                 album: '(SUMMER HOLIDAY  1963)'
+               }
+    song_info(@tag_actual).must_equal(expected)
+  end
+end
