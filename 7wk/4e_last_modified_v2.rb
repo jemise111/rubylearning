4048c74459146d1699a80e9f0ca51f72c456499f
Jesse Sessler <jesse.sessler@gmail.com>
2014-01-24 19:35:19 -0500

    simply methods
    add puts statements to running code
    add starting location option for directory list

--- 7wk/4e_last_modified_v2.rb
+++ 7wk/4e_last_modified_v2.rb
@@ -1,18 +1,22 @@
 # 4e_last_modified_v2.rb
 # Allow last_modified program to display a list of files if none is given
 
-def last_modified(file)
-  mod_time = File.open(file).mtime
-  (Time.now - mod_time) / 86400
+module TimeUnits
+  Seconds_In_Day = 60 * 60 * 24
 end
 
-def choose_file
-  puts "Select a file from current directory:\n"
-  file_list = `ls`.split
-  file_list.each_with_index do |f, i|
-    puts "%2d: #{f}" % i.next 
-  end
-  puts
+include TimeUnits
+
+def file_list(location = '.')
+  Dir.glob(location + '/' + '*')
+end
+
+def last_modified(file)
+  mod_time = File.open(file).mtime
+  (Time.now - mod_time) / TimeUnits::Seconds_In_Day
+end
+
+def file_selection(file_list)
   until (1..file_list.size).include?(selection ||= '')
     print "selection: "
     selection = gets.to_i
@@ -21,7 +25,16 @@ def choose_file
 end
 
 if __FILE__ == $PROGRAM_NAME
-  file = ARGV[0].nil? ? choose_file : ARGV[0]
+  file =
+    if ARGV[0].nil?
+      puts "Select a file from current directory:\n"
+      file_list.each_with_index do |f, i|
+        puts "%2d: #{f}" % i.next
+      end
+      file_selection(file_list)
+    else
+      ARGV[0]
+    end
   begin
     puts "file was last modified #{last_modified(file)} days ago"
   rescue Errno::ENOENT
